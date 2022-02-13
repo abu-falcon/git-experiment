c8c3f1b417f742e6b7420da85a5895e4dc6a5429
abu-falcon <nopufa89@gmail.com>
2022-02-13 16:27:53 +0400

    first commit

---
+++ pattern_matching.py
@@ -0,0 +1,29 @@
+from enum import Enum, auto
+from typing import Any
+
+
+class A(Enum):
+    one = auto()
+    two = auto()
+    three = auto()
+
+
+def give_three_args(*args: Any) -> None:
+    match args:
+        case (int(), int(), int()):
+            print("Three integer arguments!")
+        case (str(), str(), str()):
+            print("Three string arguments!")
+        case (x, y, z) if type(x) == type(y) == type(z):
+            print("Three arguments with same type")
+        case (_, _, _):
+            print("Three random arguments")
+        case _:
+            print("Not Allowed")
+
+
+give_three_args(1, 2, 3)
+give_three_args("yes", "no", "name")
+give_three_args(A.one, A.two, A.three)
+give_three_args(1, "yes", A.one)
+give_three_args(5)
